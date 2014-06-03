44c3f5631cef2f63ec1ba889145ffaa7877943a0
ssspyros <ssspyros@gmail.com>
2014-06-03 14:52:13 -0700

    Code cleanup: added drawing methods

--- jupi/CaromTable.java
+++ jupi/CaromTable.java
@@ -69,8 +69,7 @@ public class CaromTable extends JPanel
         
         balls.add(whiteball);
         balls.add(redball);
-        balls.add(yellowball);
-              
+        balls.add(yellowball);              
         
         whiteball.setVelocity(0,0);
         redball.setVelocity(0,0);
@@ -83,11 +82,9 @@ public class CaromTable extends JPanel
         mark   = BilliardsConstants.MARK;
         
         this.addMouseListener(new MouseListener()
-        {
-
-			@Override
+        {	@Override
 			public void mouseClicked(MouseEvent e) 	{}
-
+        
 			@Override
 			public void mousePressed(MouseEvent e) 
 			{
@@ -97,8 +94,7 @@ public class CaromTable extends JPanel
 			@Override
 			public void mouseReleased(MouseEvent e) 
 			{
-				mouseDown = false;
-				
+				mouseDown = false;				
 			}
 
 			@Override
@@ -126,15 +122,34 @@ public class CaromTable extends JPanel
     @Override
     protected void paintComponent(Graphics g) 
     {
-        super.paintComponent(g);
-        int x, y, width, height;
+        super.paintComponent(g);        
+        drawTable(g);
+        drawBalls(g);   
+        //Drawing cue stick         
+        if(showCue)
+        {
+        	// -----------------------------------------------------------------------------
+        	updateScore();
+        	//System.out.printf("Score = %d\n", score);
+        	System.out.printf("SwitchPlayers = " + isSwitchPlayers + "\n");//testing
+        	
+        	drawCue(g);
+        }//if        
+    }//paintComponent
+
+
+    //--------------------------------------------------------------------------------
+    /* Drawing Table */
+    //--------------------------------------------------------------------------------
+    public void drawTable(Graphics g)
+    {    	
         int cushionEdge = (int) ((floorWidth + borderWidth) * ppi);
         int tableEdge   = (int) ((floorWidth + borderWidth + cushionWidth) * ppi);
-
+        
         /* draw a filled rectangle for the floor surrounding the table border */
         g.setColor(floor);
         g.fillRect(0, 0, (int)(dimFloor[0] * ppi), (int)(dimFloor[1] * ppi));
-
+        
         /* draw a filled round rect for the rail border surrounding the cushions */
         g.setColor(border);
         g.fillRoundRect((int)(floorWidth * ppi)  , (int)(floorWidth * ppi),
@@ -152,8 +167,7 @@ public class CaromTable extends JPanel
         g.drawRect(tableEdge, tableEdge,
                    (int)(dimTable[0] * ppi),
                    (int)(dimTable[1] * ppi));
-       
-        
+               
         /* draw a line segment connecting the play field corners to each border corner */
         g.drawLine(cushionEdge, cushionEdge, tableEdge, tableEdge);
         g.drawLine(cushionEdge + (int)dimCushion[0] * ppi, cushionEdge,
@@ -192,99 +206,74 @@ public class CaromTable extends JPanel
   			       	   (int)(cushionEdge + (i*dimTable[1]/(sideMarks+1))*ppi),
   			           markRadius, markRadius);        	
         }
-        
+    }//drawTable
+    
+    
+    //--------------------------------------------------------------------------------
+    /* Drawing Balls */
+    //--------------------------------------------------------------------------------
+    public void drawBalls(Graphics g)
+    {        
+    	int x, y, width, height;    	
+    	width  = (int)(2*radius*ppi);
+    	height = (int)(2*radius*ppi);
+    	
         for (Ball b: balls) 
-        {  
-            // draw a filled circle for each ball 
+        {   
+        	x = (int)((floorWidth + borderWidth + cushionWidth + b.getPosition().x-radius) * ppi);
+        	y = (int)((floorWidth + borderWidth + cushionWidth + b.getPosition().y-radius) * ppi);        	
+        	
+        	// draw a filled circle for each ball 
             g.setColor(b.getColor());
-            g.fillOval((int)((floorWidth + borderWidth + cushionWidth + b.getPosition().x-radius) * ppi),
-                    (int)((floorWidth + borderWidth +cushionWidth + b.getPosition().y-radius) * ppi),
-                    (int)(2*radius*ppi), (int)(2*radius*ppi));
-            
+            g.fillOval(x, y, width, height);            
             // draw a circle outline for each ball 
             g.setColor(edge);
-            g.drawOval((int)((floorWidth + borderWidth + cushionWidth + b.getPosition().x-radius) * ppi),
-                    (int)((floorWidth + borderWidth +cushionWidth + b.getPosition().y-radius) * ppi),
-                    (int)(2*radius*ppi), (int)(2*radius*ppi));
-        }
-   
-        /*
-         * Drawing cue stick
-         */
-        if(showCue)
-        {
-        	// -----------------------------------------------------------------------------
-        	updateScore();
-        	//System.out.printf("Score = %d\n", score);
-        	System.out.printf("SwitchPlayers = " + isSwitchPlayers + "\n");//testing
-        	
-        	        	
-        	double dx = mouseX- ((floorWidth + borderWidth + cushionWidth+currentBall.getPosition().x) * ppi);
-        	double dy = mouseY - ((floorWidth + borderWidth + cushionWidth+currentBall.getPosition().y) * ppi);
-        
-        	double angle = Math.atan2(dy, dx) - Math.toRadians(90);
-        	cueStick.setAngle(angle);
-                	
-			cueStick.setPosition(( floorWidth + borderWidth + cushionWidth + currentBall.getPosition().x - BilliardsConstants.CUE_DIAMETER*.5 ) ,
-				(pullDistance + floorWidth + borderWidth +cushionWidth + currentBall.getPosition().y - radius + gap));	
-                
-        	int[] cuesX = new int[]{(int)(cueStick.getX()*ppi),
-        					(int)((cueStick.getX() + BilliardsConstants.CUE_DIAMETER )*ppi),
-        					(int)((cueStick.getX() +  1.5*BilliardsConstants.CUE_DIAMETER )*ppi),
-        					(int)((cueStick.getX() -  BilliardsConstants.CUE_DIAMETER*.5 )*ppi)};
-        	int[]cuesY = new int[]{(int)((cueStick.getY() + BilliardsConstants.CUE_DIAMETER*.5)*ppi),
-        					(int)((cueStick.getY() + BilliardsConstants.CUE_DIAMETER*.5)*ppi),
-        					(int)((cueStick.getY() + BilliardsConstants.CUE_DIAMETER*.5 + cueStickLength)*ppi),
-        					(int)((cueStick.getY() + BilliardsConstants.CUE_DIAMETER*.5 + cueStickLength)*ppi)};
-        
-        	path.updateProp(currentBall.getPosition(), angle);
-        	 path.draw(g,cushionWidth,floorWidth,borderWidth);
-                     
-        	AffineTransform transform = new AffineTransform();
-        
-        	transform.rotate(angle,(floorWidth + borderWidth + cushionWidth + currentBall.getPosition().x )*ppi,(floorWidth + borderWidth + cushionWidth + currentBall.getPosition().y  )*ppi);
-        	Graphics2D g2d = (Graphics2D)g;
-        	g2d.transform(transform);
-			g2d.setColor(Color.BLUE);
-			g2d.fillArc((int)(cueStick.getX()*ppi) , 
-				   (int)(cueStick.getY()*ppi),
-				   (int)(BilliardsConstants.CUE_DIAMETER*ppi) ,(int)(BilliardsConstants.CUE_DIAMETER*ppi) ,0 ,180);
-		
-			g2d.setColor(new Color(0xDBB84D));
-			g2d.fillPolygon(cuesX, cuesY, 4);
-        }//Cue
-        
-    }//paintComponent
-
-
-    public int getPixelsPerInch() 
-    {
-        return ppi;
-    }
-
-    public void setPixelsPerInch(int pixelsPerInch) 
-    {
-        if (pixelsPerInch < 1)
-            throw new IllegalArgumentException("Pixels per inch must be positive.");
-        this.ppi = pixelsPerInch;
-    }
+            g.drawOval(x, y, width, height);
+        }    	
+    }//drawBalls
     
-    //Get Table Dimensions
-    public double getTableWidth()
-    {
-    	return dimTable[0];
-    }
     
-    public double getTableHeight()
+    //--------------------------------------------------------------------------------
+    /* Drawing Cue Stick */
+    //--------------------------------------------------------------------------------
+    public void drawCue(Graphics g)
     {
-    	return dimTable[1];
-    }
-
-    @Override
-    public Dimension getPreferredSize() 
-    {
-        return new Dimension((int)(dimFloor[0] * ppi), (int)(dimFloor[1] * ppi));
-    }
+    	double dx = mouseX- ((floorWidth + borderWidth + cushionWidth+currentBall.getPosition().x) * ppi);
+    	double dy = mouseY - ((floorWidth + borderWidth + cushionWidth+currentBall.getPosition().y) * ppi);
+    
+    	double angle = Math.atan2(dy, dx) - Math.toRadians(90);
+    	cueStick.setAngle(angle);
+            	
+		cueStick.setPosition(( floorWidth + borderWidth + cushionWidth + currentBall.getPosition().x - BilliardsConstants.CUE_DIAMETER*.5 ) ,
+			(pullDistance + floorWidth + borderWidth +cushionWidth + currentBall.getPosition().y - radius + gap));	
+            
+    	int[] cuesX = new int[]{(int)(cueStick.getX()*ppi),
+    					(int)((cueStick.getX() + BilliardsConstants.CUE_DIAMETER )*ppi),
+    					(int)((cueStick.getX() +  1.5*BilliardsConstants.CUE_DIAMETER )*ppi),
+    					(int)((cueStick.getX() -  BilliardsConstants.CUE_DIAMETER*.5 )*ppi)};
+    	int[]cuesY = new int[]{(int)((cueStick.getY() + BilliardsConstants.CUE_DIAMETER*.5)*ppi),
+    					(int)((cueStick.getY() + BilliardsConstants.CUE_DIAMETER*.5)*ppi),
+    					(int)((cueStick.getY() + BilliardsConstants.CUE_DIAMETER*.5 + cueStickLength)*ppi),
+    					(int)((cueStick.getY() + BilliardsConstants.CUE_DIAMETER*.5 + cueStickLength)*ppi)};
+    
+    	path.updateProp(currentBall.getPosition(), angle);
+    	 path.draw(g,cushionWidth,floorWidth,borderWidth);
+                 
+    	AffineTransform transform = new AffineTransform();
+    
+    	transform.rotate(angle,(floorWidth + borderWidth + cushionWidth + currentBall.getPosition().x )*ppi,(floorWidth + borderWidth + cushionWidth + currentBall.getPosition().y  )*ppi);
+    	Graphics2D g2d = (Graphics2D)g;
+    	g2d.transform(transform);
+		g2d.setColor(Color.BLUE);
+		g2d.fillArc((int)(cueStick.getX()*ppi) , 
+			   (int)(cueStick.getY()*ppi),
+			   (int)(BilliardsConstants.CUE_DIAMETER*ppi) ,(int)(BilliardsConstants.CUE_DIAMETER*ppi) ,0 ,180);
+	
+		g2d.setColor(new Color(0xDBB84D));
+		g2d.fillPolygon(cuesX, cuesY, 4);    	
+    }//drawCue
+    
+        
 
     public void update()
     {
@@ -308,9 +297,7 @@ public class CaromTable extends JPanel
 		}
 */		
 		//Check if balls are at rest
-		if(whiteball.getVelocity().x ==0 && whiteball.getVelocity().y == 0
-				&& yellowball.getVelocity().x == 0 && yellowball.getVelocity().y  == 0
-				&& redball.getVelocity().x == 0&& redball.getVelocity().y == 0)
+    	if (isBallsStopped())		
 		{
 			showCue = true;			
 //*		
@@ -375,7 +362,7 @@ public class CaromTable extends JPanel
     			ball2 = balls.get(j);
     			
     			if (Physics.checkBallCollision(ball1, ball2))
-    			{
+    			{//had a collision
     				if (ball1.isCurrentBall() == true)
     				{ 
     					checkIfScored(ball2);    								
@@ -387,12 +374,18 @@ public class CaromTable extends JPanel
     			}
     		}//for j
     		balls.get(i).update();
-    		
-    		
     	}//for i
     	
     	repaint();
     }//update
+    
+    
+    public boolean isBallsStopped()
+    {
+    	return ( whiteball.getVelocity().x  == 0 && whiteball.getVelocity().y   == 0 &&
+				 yellowball.getVelocity().x == 0 && yellowball.getVelocity().y  == 0 &&
+				 redball.getVelocity().x    == 0 && redball.getVelocity().y     == 0);    	
+    }
     
     
     public void checkIfScored(Ball ball)
@@ -462,4 +455,33 @@ public class CaromTable extends JPanel
 			isSwitchPlayers = false; //reset
     }//switchPlayers
     
+    
+    public int getPixelsPerInch() 
+    {
+        return ppi;
+    }
+
+    public void setPixelsPerInch(int pixelsPerInch) 
+    {
+        if (pixelsPerInch < 1)
+            throw new IllegalArgumentException("Pixels per inch must be positive.");
+        this.ppi = pixelsPerInch;
+    }
+    
+    //Get Table Dimensions
+    public double getTableWidth()
+    {
+    	return dimTable[0];
+    }
+    
+    public double getTableHeight()
+    {
+    	return dimTable[1];
+    }
+
+    @Override
+    public Dimension getPreferredSize() 
+    {
+        return new Dimension((int)(dimFloor[0] * ppi), (int)(dimFloor[1] * ppi));
+    }    
 }
